4d92effda7ec126816374f232665b1aa39bae188
International <internationalaccessschool@gmail.com>
2026-04-17 11:55:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
                 safari_web_id: "web.onesignal.auto.10485988-1822-4e96-b399-29edb7cde282",
                 notifyButton: { enable: false },
                 allowLocalhostAsSecureOrigin: true,
+                serviceWorkerPath: "/OneSignalSDKWorker.js",
               });
             });
           `}
